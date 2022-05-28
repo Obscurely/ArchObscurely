3a55554cf5a48fc09ee456cf22ccc3f0d0cf28a6
Obscurely <59087558+Obscurely@users.noreply.github.com>
2022-05-28 09:41:03 +0300

    Optimize update script for poly bar (lighter load)

--- dotfiles/polybar/default/scripts/updates.sh
+++ dotfiles/polybar/default/scripts/updates.sh
@@ -1,28 +1,12 @@
 #!/usr/bin/env bash
 
-NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
-
 get_total_updates() { UPDATES=$(checkupdates 2>/dev/null | wc -l); }
 
 while true; do
     get_total_updates
 
-    # notify user of updates
-    if hash notify-send &>/dev/null; then
-        if (( UPDATES > 50 )); then
-            notify-send -u critical -i $NOTIFY_ICON \
-                "You really need to update!!" "$UPDATES New packages"
-        elif (( UPDATES > 25 )); then
-            notify-send -u normal -i $NOTIFY_ICON \
-                "You should update soon" "$UPDATES New packages"
-        elif (( UPDATES > 2 )); then
-            notify-send -u low -i $NOTIFY_ICON \
-                "$UPDATES New packages"
-        fi
-    fi
-
     # when there are updates available
-    # every 10 seconds another check for updates is done
+    # every half an hour another check for updates is done
     while (( UPDATES > 0 )); do
         if (( UPDATES == 1 )); then
             echo " $UPDATES"
@@ -31,15 +15,15 @@ while true; do
         else
             echo " None"
         fi
-        sleep 10
+        sleep 1800
         get_total_updates
     done
 
     # when no updates are available, use a longer loop, this saves on CPU
-    # and network uptime, only checking once every 30 min for new updates
+    # and network uptime, only checking once every hour for new updates
     while (( UPDATES == 0 )); do
         echo " None"
-        sleep 1800
+        sleep 3600
         get_total_updates
     done
 done
